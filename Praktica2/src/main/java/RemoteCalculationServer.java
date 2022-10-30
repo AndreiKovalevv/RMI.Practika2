80e727adbbedea68e96283bae123cad963c30289
AndreiKovalevv <kovaliv.andrey007@mail.ru>
2022-10-30 23:40:30 +0400

    Практика 2

---
+++ Praktica2/src/main/java/RemoteCalculationServer.java
@@ -0,0 +1,11 @@
+import java.rmi.RemoteException;
+
+public class RemoteCalculationServer implements Calculator {
+
+   @Override
+   public int multiply(int x, int y) throws RemoteException {
+       return x*y;    
+   }
+     
+
+}
